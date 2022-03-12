283fdd281c7a5913d9bc5b31718bf1a3eb9dee13
logit <7jiung@gmail.com>
2022-03-12 13:49:21 +0900

    반복문 작업
    반복문 작업으로 다양한 기능을 사용 할 수 있도록 추가

--- 봉사시간관리/middle_school_VOLUNTEER_WORK.py
+++ 봉사시간관리/middle_school_VOLUNTEER_WORK.py
@@ -1,11 +1,31 @@
 from asyncore import write
 from inspect import formatannotationrelativeto
+from re import T
 import tkinter
 import pickle
+
 file_name = "봉사시간관리/Workedtime.pickle"
-with open(file_name, "rb") as r:
-    retime = pickle.load(r)
-    time = input("몇 시간동안 봉사를 했습니까?")
-    ltime = int(retime) + int(time)
-with open(file_name, "wb") as w:
-    pickle.dump(str(ltime), w)
+
+while True:
+    print("1. 봉사시간 추가")
+    print("2. 현재 봉사시간 조회")
+    print("3. 프로그램 종료")
+    awnser = input("어느 것을 할 것인가요?")    
+    if awnser == "봉사시간 추가":
+        with open(file_name, "rb") as r:
+            retime = pickle.load(r)
+            time = input("몇 시간동안 봉사를 했습니까?")
+            ltime = int(retime) + int(time)
+        with open(file_name, "wb") as w:
+            pickle.dump(str(ltime), w)
+            continue
+    elif awnser == "현재 봉사시간 조회":
+        with open(file_name, "rb") as r:
+            readtime = pickle.load(r)
+            print("현재 봉사 시간은 {0}시간 입니다".format(readtime))
+            continue
+    elif awnser == "프로그램 종료":
+        break
+    else:
+        print("다시 입력해 주세요")
+        continue
